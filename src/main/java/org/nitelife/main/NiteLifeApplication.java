b859b1d257947520b7c51968f58a9d3ef1dc6876
Fnu Ferry <fnuferry6@gmail.com>
2025-04-23 15:55:28 -0700

    init

---
+++ src/main/java/org/nitelife/main/NiteLifeApplication.java
@@ -0,0 +1,13 @@
+package org.nitelife.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NiteLifeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NiteLifeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/nitelife/main/NiteLifeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.nitelife.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NiteLifeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
